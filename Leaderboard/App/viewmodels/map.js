13633dd55a9ee108aa22cb8432015fc56e64d842
Josh Crawford <josh@crawford.cc>
2013-06-28 14:41:18 -0700

    Added leaderboard view, updated competitions view, updated boostrap with a color scheme

--- Leaderboard/App/viewmodels/map.js
+++ Leaderboard/App/viewmodels/map.js
@@ -1,24 +1,46 @@
-﻿define(['services/datacontext'], function (datacontext) {
-    var athletes = ko.observableArray();
-    var initialized = false;
+﻿define(['services/datacontext', 'durandal/plugins/router', 'durandal/app'], function (datacontext, router, app) {
+    var competitions = ko.observableArray();
+
+    function activate() {
+        return datacontext.getCompetitions(competitions);
+    }
+
+    var deactivate = function () {
+        competitions([]);
+    };
+
+    function refresh() {
+        return datacontext.getCompetitions(competitions);
+    }
+
+    var gotoDetails = function (selectedCompetition) {
+        if (selectedCompetition && selectedCompetition.competitionId()) {
+            var url = '#/competitiondetail/' + selectedCompetition.competitionId();
+            router.navigateTo(url);
+        }
+    };
+
+    var viewAttached = function (view) {
+        bindEventToList(view, '.competition-brief', gotoDetails);
+    };
+
+    var bindEventToList = function (rootSelector, selector, callback, eventName) {
+        var eName = eventName || 'click';
+        $(rootSelector).on(eName, selector, function () {
+            var competition = ko.dataFor(this);
+            callback(competition);
+            return false;
+        });
+    };
+
     var vm = {
         activate: activate,
-        athletes: athletes,
-        title: 'Athletes',
-        refresh: refresh
+        competitions: competitions,
+        deactivate: deactivate,
+        refresh: refresh,
+        title: 'Competitions',
+        viewAttached: viewAttached
     };
 
     return vm;
-
-    function activate() {
-        if (initialized) {
-            return;
-        }
-        initialized = true;
-        return refresh();
-    }
-
-    function refresh () {
-        return datacontext.getAthletes(athletes);
-    }
-    });
+});
